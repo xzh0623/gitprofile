bd837f170dc6ccf062219aa0310fd0c5fc803a1d
Xiang-Zhen Chen <ss612270623@gmail.com>
2024-11-19 16:04:06 +0800

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -145,11 +145,11 @@ const CONFIG = {
   //   },
   ],
   // Display articles from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'xzh0623', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
-  },
+  // blog: {
+  //   source: 'dev', // medium | dev
+  //   username: 'xzh0623', // to hide blog section, keep it empty
+  //   limit: 1, // How many articles to display. Max is 10.
+  // },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
