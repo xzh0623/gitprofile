b738d8fa3ce3c5cd25fd72d1e161c87e32ea9021
Xiang-Zhen Chen <ss612270623@gmail.com>
2024-11-19 16:25:34 +0800

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -62,9 +62,9 @@ const CONFIG = {
     mastodon: '',
     researchGate: '',
     facebook: '',
-    instagram: 'xzh_0623',
+    instagram: 'xzh_06.23',
     reddit: '',
-    threads: 'xzh_0623',
+    threads: 'xzh_06.23',
     youtube: '', // example: 'pewdiepie'
     udemy: '',
     dribbble: '',
@@ -142,7 +142,7 @@ const CONFIG = {
       // link: 'https://example.com',
     //   description:
     //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    // },
+    },
   ],
   // Display articles from your medium or dev account. (Optional)
   // blog: {
