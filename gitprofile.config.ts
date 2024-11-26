a559f1217de902e4ba0ff136993cf5a9bb02e45d
Xiang-Zhen Chen <ss612270623@gmail.com>
2024-11-26 16:05:07 +0800

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -80,7 +80,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/17SY-HZpC-kaBs7YBCYFmmI-hJPmStT5e/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'PHP',
